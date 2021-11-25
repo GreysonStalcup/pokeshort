3a79428067a71ba443abe11030a10bfa17783b03
GreysonStalcup <g_stalcup@hotmail.com>
2021-11-24 23:41:45 -0800

    added PHP wrapper

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '639fefcdf442be7bdc44d208a1e0ee3547d21b06',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '639fefcdf442be7bdc44d208a1e0ee3547d21b06',
+            'dev_requirement' => false,
+        ),
+        'danrovito/pokephp' => array(
+            'pretty_version' => '1.3',
+            'version' => '1.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../danrovito/pokephp',
+            'aliases' => array(),
+            'reference' => 'd2a1d5a97f241a249a5f101d80042f3a4bf9d430',
+            'dev_requirement' => false,
+        ),
+    ),
+);
